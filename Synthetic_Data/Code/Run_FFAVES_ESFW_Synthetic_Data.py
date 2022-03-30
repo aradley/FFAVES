f19970c41449a6d091b5d41e501e1deafd38a14d
aradley <arthur_radley@hotmail.co.uk>
2022-03-30 11:11:53 +0100

    Add files via upload

--- Synthetic_Data/Code/Run_FFAVES_ESFW_Synthetic_Data.py
+++ Synthetic_Data/Code/Run_FFAVES_ESFW_Synthetic_Data.py
@@ -2,8 +2,8 @@
 # to identify spurious data points according to the prevailing structure in the data.
 
 ### Set folder path ###
-path = "/mnt/c/Users/arthu/OneDrive - University of Cambridge/Entropy_Sorting_Paper_2022/"
-path_2 = "Synthetic_Data/Objects_For_Paper/"
+path = "/Users/aradley/Python_Folders/Enviroments/FFAVES_Test/FFAVES"
+path_2 = "/Synthetic_Data/Objects_From_Paper/"
 ###
 
 ### Import packages ###
@@ -38,32 +38,32 @@ for i in np.arange(Dicretisation_Cutoffs.shape[0]):
 ## Auto_Save set to False so not to overwrite objects used for paper or save in undersired directory.
 ## Discretised_Data should be a numpy array of 0's and 1's with the rows as samples and the columns as features.
 # FFAVES
-Track_Imputation_Steps, Track_Percentage_Imputation = ffaves.FFAVES(Discretised_Data,Auto_Save=False)
+Track_Imputation_Steps, Track_Percentage_Imputation = ffaves.FFAVES(Discretised_Data)
 # Calculate correlation matricies
 Chosen_Cycle = -1
-Sort_Gains, Sort_Weights, Cycle_Suggested_Imputations, ES_Matrices_Features_Used_Inds = ffaves.Calculate_ES_Sort_Matricies(Discretised_Data, Track_Imputation_Steps, Chosen_Cycle=Chosen_Cycle,Auto_Save=False)
+Sort_Gains, Sort_Weights, Cycle_Suggested_Imputations, ES_Matrices_Features_Used_Inds = ffaves.Calculate_ES_Sort_Matricies(Discretised_Data, Track_Imputation_Steps, Chosen_Cycle=Chosen_Cycle)
 # ESFW
-Feature_Divergences, Cycle_Suggested_Imputations, Feature_Divergences_Used_Inds = ffaves.ESFW(0.1, Discretised_Data, Track_Imputation_Steps, Chosen_Cycle=Chosen_Cycle,Auto_Save=False)
+Feature_Divergences, Cycle_Suggested_Imputations, Feature_Divergences_Used_Inds = ffaves.ESFW(0.1, Discretised_Data, Track_Imputation_Steps, Chosen_Cycle=Chosen_Cycle)
 # Feature weights
 Mean_Feature_Divergences = np.mean(Feature_Divergences,axis=0)
 # Identify optimised discretisation thresholds based on the discretised matrix before and after application of FFAVES. 
 # This is a cursory function for demonstration of FFAVES's ability to account for sub-optimal discretisation. It is an optional step in the
 # suggested FFAVES workflow and was primarily created for synthetic data with known ground truth rather than complex real data.
-Optimal_Thresholds = ffaves.Parallel_Optimise_Discretisation_Thresholds(np.asarray(Drop_Out_Synthetic_Data).astype("f"),Discretised_Data.astype("f"),Cycle_Suggested_Imputations,Auto_Save=False)
+Optimal_Thresholds = ffaves.Parallel_Optimise_Discretisation_Thresholds(np.asarray(Drop_Out_Synthetic_Data).astype("f"),Discretised_Data.astype("f"),Cycle_Suggested_Imputations)
 
 ### Load objects for basic plotting ###
 # Logs the how many points in the Discretised_Data have been suggested as false negative (FN) and false positive (FP) data points after each itteration of FFAVES.
-Track_Percentage_Imputation = np.load(path+path_2+"Track_Percentage_Imputation.npy")
+Track_Percentage_Imputation = np.load("Track_Percentage_Imputation.npy")
 # The Sort Gain (SG) and Sort Weight (SW) scores for each pair of features in the data, for the given cycle of FFAVES that
 # was used for suggesting FN/FP data points (Chosen_Cycle = -1 indicates the last cycle of FFAVES). Technically the Sort_Gains matrix
 # is a product of the Sort Gain and Sort Direction scores, hence values can range from -1 to 1. This was done to save memory. To obtain the true
 # Sort Gain and Sort Direction scores, extract all negative values as -1 and all positive values as 1 and then get 
 # the absolute values of the Sort_Gains matrix.
-Sort_Gains = np.load(path+path_2+"Sort_Gains.npy")
-Sort_Weights = np.load(path+path_2+"Sort_Weights.npy")
+Sort_Gains = np.load("Sort_Gains.npy")
+Sort_Weights = np.load("Sort_Weights.npy")
 # IDs all of the feature columns that were used. Indicies that are not present are features that were exlcuded because the number of minority states
 # observed in the feature was less than Min_Clust_Size (default = 5).
-ES_Matrices_Features_Used_Inds = np.load(path+path_2+"ES_Matrices_Features_Used_Inds.npy")
+ES_Matrices_Features_Used_Inds = np.load("ES_Matrices_Features_Used_Inds.npy")
 
 ### Plot basic plots ###
 # Plot the convergence of suggested FN/FP data points
